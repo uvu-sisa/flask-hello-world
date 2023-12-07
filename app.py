37ad3ffc6eb4c5e50b42f97eb594f482a894c8cb
uvu <uyvq.ssu@gmail.com>
2023-12-06 22:14:58 -0500

    add new index

--- app.py
+++ app.py
@@ -152,7 +152,9 @@ csv_file_path = 'numbers.csv'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    prediction_list=[]
     if request.method == 'POST':
+        
         if 'clear' in request.form:
             # Clear the CSV file
             os.remove('*.csv')
@@ -164,6 +166,18 @@ def index():
             with open(csv_file_path, 'a', newline='') as csvfile:
                 csv_writer = csv.writer(csvfile)
                 csv_writer.writerow([number])
+            pred = np.zeros(NUM_INS)
+            if number==37:number='0'
+            if number==38:number='00'
+            new_df = create_data(number)
+            actual_df = write_data(new_df,'actual')
+            last_pred = get_last_predictor()
+            period_pnl = cal_period_pnl(last_pred,new_df.values.reshape(-1))
+            pred = gen_predictor(actual_df)
+            write_data(pred,'predictor')
+            predT= pred.T
+            prediction_list = predT.index.values
+            write_pnl(period_pnl)    
             
     # Read the existing numbers from the CSV file
     existing_numbers = []
@@ -171,21 +185,13 @@ def index():
         csv_reader = csv.reader(csvfile)
         for row in csv_reader:
             existing_numbers.append(row[0])
-    pred = np.zeros(NUM_INS)
-    if number==37:number='0'
-    if number==38:number='00'
-    new_df = create_data(number)
-    actual_df = write_data(new_df,'actual')
-    last_pred = get_last_predictor()
-    period_pnl = cal_period_pnl(last_pred,new_df.values.reshape(-1))
-    pred = gen_predictor(actual_df)
-    write_data(pred,'predictor')
-    predT= pred.T
     
-    write_pnl(period_pnl)    
     # Sample data
-    pnls = pd.read_csv('pnl.csv')
-    prediction_list = predT.index.values
+    if os.path.exists('pnl.csv'):
+        pnls = pd.read_csv('pnl.csv')
+    else:
+        pnls = pd.DataFrame()
+    
     # Create a Plotly Express plot
     fig = px.line(pnls, x="number", y="pnl", title='Cum PNL')
 
