2ab5a65686dcbfa0ecba8c1e07ce07ec0b3233da
uvu <uyvq.ssu@gmail.com>
2023-12-06 22:01:28 -0500

    add new index

--- core.py
+++ core.py
@@ -2,7 +2,21 @@ import os
 '''
 new data -> create actual data -> save_actual data -> create prediction -> new data ->save actual data, create pnl -> save pnl
 '''
+rednums = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+blacknums = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 
+NUMIX = list(np.arange(1,37))
+NUM_MASK = np.zeros(len(NUMIX))
+EVENTIX = ['even','odd','black','red','3div','3div1','3div2','L','H',]
+EVENT_MASK = np.zeros(len(EVENTIX))
+QIX =['1q','2q','3q']
+Q_MASK = np.zeros(len(QIX))
+ZEROSIX =['0','00']
+ZEROS_MASK = np.zeros(len(ZEROSIX))
+TICKER = NUMIX+EVENTIX+QIX+ZEROSIX
+TICKERIX = np.arange(0,len(TICKER)+1)
+NUM_INS = len(TICKER)
+RETURNS = [36]*len(NUMIX)+[2]*len(EVENTIX)+[3]*len(QIX)+[18]*len(ZEROSIX)
 
 HL_H = np.arange(19,37)
 HL_L = np.arange(1,19)
@@ -64,13 +78,14 @@ def create_data(x):
         data[-1] = 1
         return pd.DataFrame(data,index=TICKER).T
     data[x-1] = 1 # number
-    data[36+1+x%2] = 1 #event odd
+    data[36+x%2] = 1 #event odd
     data[36+2+int(x in (rednums))] = 1 #black n red
     data[36+2+2+x%3] = 1 # div 3
     data[36+2+2+3+get_lh_idx(x)] = 1 # LH
     data[36+2+2+3+2+get_lmh_idx(x)] = 1 # lmh 1q 2q 3q
     
     return pd.DataFrame(data,index=TICKER).T
+
 
 def write_data(new_df,module_name):
     '''module_name can be actual, prediction'''
