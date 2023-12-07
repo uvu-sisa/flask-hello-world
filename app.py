2f1079fdfdf7de879e15e95af0b7a28a021e039f
uvu <uyvq.ssu@gmail.com>
2023-12-06 21:46:45 -0500

    add new index

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import csv
 import plotly.express as px
 import plotly.io as pio
 import io
+from .core import *
 
 app = Flask(__name__)
 
@@ -14,26 +15,40 @@ def index():
     if request.method == 'POST':
         if 'clear' in request.form:
             # Clear the CSV file
+            os.remove('*.csv')
             open(csv_file_path, 'w').close()
+            
         else:
             # Get the number from the form and append it to the CSV file
             number = request.form['number']
             with open(csv_file_path, 'a', newline='') as csvfile:
                 csv_writer = csv.writer(csvfile)
                 csv_writer.writerow([number])
+            
     # Read the existing numbers from the CSV file
     existing_numbers = []
     with open(csv_file_path, 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
         for row in csv_reader:
             existing_numbers.append(row[0])
+    pred = np.zeros(NUM_INS)
+    if number==37:number='0'
+    if number==38:number='00'
+    new_df = create_data(number)
+    actual_df = write_data(new_df,'actual')
+    last_pred = get_last_predictor()
+    period_pnl = cal_period_pnl(last_pred,new_df.values.reshape(-1))
+    pred = gen_predictor(actual_df)
+    write_data(pred,'predictor')
+    predT= pred.T
     
+    write_pnl(period_pnl)    
     # Sample data
-    df = px.data.iris()
-
+    pnls = pd.read_csv('pnl.csv')
+    prediction_list = predT.index.values
     # Create a Plotly Express plot
-    fig = px.scatter(df, x='sepal_width', y='sepal_length', color='species', size='petal_length')
+    fig = px.line(pnls, x="number", y="pnl", title='Cum PNL')
 
     # Convert the plot to HTML
     plot_html = pio.to_html(fig, full_html=False)
-    return render_template('index.html', existing_numbers=existing_numbers,plot_html=plot_html)
+    return render_template('index.html',prediction_list=prediction_list, existing_numbers=existing_numbers,plot_html=plot_html)
